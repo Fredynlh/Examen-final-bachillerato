e90774099b2e5ce02384a5f82be4fe8fb96d6baa
Fredynlh <156869313+Fredynlh@users.noreply.github.com>
2025-09-15 22:51:07 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("hola")
+const menuIcon = document.querySelector('.icono_menu');
+const navUl = document.querySelector('.nav-contenedor_ul');
+
+menuIcon.addEventListener('click', () => {
+    if(navUl.style.display === 'flex') {
+        navUl.style.display = 'none';
+    } else {
+        navUl.style.display = 'flex';
+        navUl.style.flexDirection = 'column'; // Para móvil
+    }
+});
+
